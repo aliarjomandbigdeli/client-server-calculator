987df0dc63f6fcd08cd0d6b98bbba8c643b1e127
aliarjomandbigdeli <aliabigdeli@gmail.com>
2018-12-19 12:05:44 +0330

    initiate tcp connection

---
+++ README.md
@@ -0,0 +1,2 @@
+# client-server-calculator
+It calculate query on the server side one sends the result to the client side.
--- src/Client.java
+++ src/Client.java
@@ -6,6 +6,22 @@ import java.net.Socket;
 public class Client {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        try (Socket server = new Socket("127.0.0.1", 7654)) {
+            System.out.println("Connected to server.");
+            OutputStream out = server.getOutputStream();
+            InputStream in = server.getInputStream();
+            byte[] buffer = new byte[2048];
+            String[] messages = {"Hello", "How-are-you?", "what's-up?"};
+            for (String msg: messages) {
+                out.write(msg.getBytes());
+                System.out.println("SENT: " + msg);
+                int read = in.read(buffer);
+                System.out.println("RECV: " + new String(buffer, 0, read));
+            }
+            System.out.print("All messages sent.\nClosing ... ");
+        } catch (IOException ex) {
+            System.err.println(ex);
+        }
+        System.out.println("done.");
     }
 }
